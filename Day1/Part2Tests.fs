2f09bcf3904315a08dd4b3e47602ef2edcc024c0
Patrick Drechsler <patrick@pdrechsler.de>
2022-12-01 10:27:20 +0100

    solve day1/part2

---
+++ Day1/Part2Tests.fs
@@ -0,0 +1,24 @@
+﻿module Part2Tests
+
+open Xunit
+open Swensen.Unquote
+
+open Part1Tests
+
+[<Fact>]
+let ``Input data parsing works - sum of top 3 elves`` () =
+    let entries = System.IO.File.ReadAllLines(input) |> List.ofSeq
+    let seperator = ""
+    let actual =
+        splitBy seperator entries
+        |> List.map(fun elf -> 
+            elf 
+            |> List.map(fun y -> y |> int) 
+            |> List.sum)
+        |> List.sort
+        |> List.rev
+        |> List.take 3
+        |> List.sum
+    
+    let expected = 211447
+    expected =! actual
