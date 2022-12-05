916d112cbee99671475feab7ff6a48871b66a658
Patrick Drechsler <patrick@pdrechsler.de>
2022-12-06 00:29:45 +0100

    day5/part1 WIP

--- Day5/Part1Tests.fs
+++ Day5/Part1Tests.fs
@@ -1,5 +1,6 @@
 module Part1Tests
 
+open System
 open System.Text.RegularExpressions
 open Xunit
 open Swensen.Unquote
@@ -17,16 +18,7 @@ let sampleData =
         "move 1 from 1 to 2"
     ]
 
-type CrateId = char
-type Crate = CrateId list
-type StackId = int
-type Stack = {
-    Number: StackId
-    Crates: Crate list
-}
-
-let getHeader data = data |> List.takeWhile (fun x -> x <> "")
-        
+    
 /// Examples:
 /// 
 /// 1 -> 1
@@ -37,8 +29,7 @@ let getHeader data = data |> List.takeWhile (fun x -> x <> "")
 /// 6 -> 21
 let indexOfStackNumber n = (n - 1) * 3 + n
 
-/// Extract max number of stacks
-let getNumberOfStacks header =
+let getStackIds header =
 
     let bottomRow : string =
         header
@@ -51,7 +42,44 @@ let getNumberOfStacks header =
         |> Array.head
         |> int
     
-    numberOfStacks
+    [1..numberOfStacks]
+
+type CrateId = char
+type Crate = CrateId list
+type StackId = int
+type Stack = {
+    Number: StackId
+    Crates: Crate list
+}
+
+let tryGetStack (c:char) : char option =
+    match c with
+    | Char.IsUpper x -> Some x
+    | _ -> None
+
+let tryGetStack' (c:char) : char option =
+    match c with
+    | x when Char.IsUpper x -> Some x
+    | _ -> None
+
+let tryGetStack'' (c:char) : char option =
+    match Char.IsUpper c with
+    | true -> Some c
+    | _ -> None
+    
+let getHeader data = data |> List.takeWhile (fun x -> x <> "")
+
+let addCratesToStack (previousStacks: Stack list) (row: string) : Stack list =
+    let cs = row |> List.ofSeq
+    
+    
+    // previousStacks
+    // |> List.map (fun stack -> stack.Number)
+    // |> List.map indexOfStackNumber
+    // |> List.map (fun stackId ->
+    //     let value = cs[stackId]
+    //     
+    previousStacks
     
     
 [<Fact>]
@@ -59,7 +87,7 @@ let ``Playground - get starting stack of crates`` () =
     
     let header = getHeader sampleData
 
-    let numberOfStacks = getNumberOfStacks header
+    let stackIds = getStackIds header
     
     let invertedStackRows =
         header
